9d80a1c07f4989e794fe5280004681ddea3dcff5
sean <sean.zinger1999@gmail.com>
2026-05-20 17:09:33 +0300

    Write a test program

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,6 +1,25 @@
+
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
-void app_main(void)
-{
+#define RED_LED_PIN GPIO_NUM_4 
 
+static const char *TAG = "BLINK_TEST";
+
+void app_main(void) {
+    
+    gpio_set_direction(RED_LED_PIN, GPIO_MODE_OUTPUT);
+    
+    int ledStatus = 0;
+    while (1) {
+        //ESP_LOGI(TAG, "Setting LED state to: %d", ledStatus);
+
+        gpio_set_level(RED_LED_PIN, ledStatus);
+        ledStatus = !ledStatus;
+
+        vTaskDelay(pdMS_TO_TICKS(16)); // ~31Hz
+    }
 }
