da0cc3e7420d2d58da6040562fc5612842146ac3
sz19999 <sean.zinger1999@gmail.com>
2026-05-23 16:57:54 +0300

    Write a temp motor test code in main

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,25 +1,34 @@
-
-#include <stdio.h>
-#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
+#include "driver/gpio.h"
 
-#define RED_LED_PIN GPIO_NUM_4 
+#define LED_PIN   GPIO_NUM_4
+#define STEP_PIN  GPIO_NUM_2
+#define DIR_PIN   GPIO_NUM_1
 
-static const char *TAG = "BLINK_TEST";
-
-void app_main(void) {
+void app_main(void)
+{
+    gpio_set_direction(STEP_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(DIR_PIN, GPIO_MODE_OUTPUT);
     
-    gpio_set_direction(RED_LED_PIN, GPIO_MODE_OUTPUT);
-    
-    int ledStatus = 0;
+    // turn on MCU power status LED
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, 1); 
+
+    int steps = 0;
+    int dir = 1;
     while (1) {
-        //ESP_LOGI(TAG, "Setting LED state to: %d", ledStatus);
+        gpio_set_level(STEP_PIN, 0);
+        esp_rom_delay_us(1000); // wait 1ms
+        
+        gpio_set_level(STEP_PIN, 1);
+        esp_rom_delay_us(1000); // wait 1ms
 
-        gpio_set_level(RED_LED_PIN, ledStatus);
-        ledStatus = !ledStatus;
+        steps++;
 
-        vTaskDelay(pdMS_TO_TICKS(16)); // ~31Hz
+        if (steps % 1600 == 0) {
+            dir = !dir;
+            gpio_set_level(DIR_PIN, dir);
+        }
     }
 }
