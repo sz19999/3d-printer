eb29afd205908677bff92a9df24011a84e440356
sz19999 <sean.zinger1999@gmail.com>
2026-07-30 18:29:04 +0300

    Add additional configuration parameters

--- config.h
+++ config.h
@@ -9,13 +9,20 @@
 #define EFF_EX_GEAR_D  11   // need to verify the extruder gear diameter   
 #define PI             3.14159265f
 
-#define STEPS_PER_MM_BELT  ((uint32_t)((360  * MICROSTEPS) / (STEP_ANGLE_DEG * PULLEY_TEETH * BELT_PITCH)))
-#define STEPS_PER_MM_SCREW ((uint32_t)((360  * MICROSTEPS) / (STEP_ANGLE_DEG * SCREW_LEAD)))
-#define STEPS_PER_MM_GEAR  ((uint32_t)((360 * MICROSTEPS) / (EFF_EX_GEAR_D * PI)))
+#define STEPS_PER_MM_BELT  ((float)((360  * MICROSTEPS) / (STEP_ANGLE_DEG * PULLEY_TEETH * BELT_PITCH)))
+#define STEPS_PER_MM_SCREW ((float)((360  * MICROSTEPS) / (STEP_ANGLE_DEG * SCREW_LEAD)))
+#define STEPS_PER_MM_GEAR  ((float)((360 * MICROSTEPS) / (EFF_EX_GEAR_D * PI)))
 
-#define MAX_VELOCITY_X  100.0f // 100 mm/s
-#define MAX_VELOCITY_Y  100.0f
-#define MAX_VELOCITY_Z  100.0f
-#define MAX_VELOCITY_E  100.0f
+#define MAX_VELOCITY_X  100.0f / 60.0f  // 100 mm/s
+#define MAX_VELOCITY_Y  100.0f / 60.0f
+#define MAX_VELOCITY_Z  100.0f / 60.0f
+#define MAX_VELOCITY_E  100.0f / 60.0f
+
+#define MAX_ACCELERATION_X  100 // need to verify each acceleration constant and velocity
+#define MAX_ACCELERATION_Y  100
+#define MAX_ACCELERATION_Z  100
+#define MAX_ACCELERATION_E  100
+
+#define JUNCTION_DEVIATION  0.02f // 0.02mm - need to test
 
 #endif
