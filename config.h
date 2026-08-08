652d589b6e7da9cd6f1f4e8a1ef4650acaa6e660
sz19999 <sean.zinger1999@gmail.com>
2026-08-08 12:59:07 +0300

    feat(motion planner): Edit config file

--- config.h
+++ config.h
@@ -18,10 +18,10 @@
 #define MAX_VELOCITY_Z  3500.0f / 60.0f
 #define MAX_VELOCITY_E  3500.0f / 60.0f
 
-#define MAX_ACCELERATION_X  300 // need to verify each acceleration constant and velocity
-#define MAX_ACCELERATION_Y  300
-#define MAX_ACCELERATION_Z  300
-#define MAX_ACCELERATION_E  300
+#define MAX_ACCELERATION_X  10.0f // need to verify each acceleration constant and velocity
+#define MAX_ACCELERATION_Y  10.0f
+#define MAX_ACCELERATION_Z  10.0f
+#define MAX_ACCELERATION_E  10.0f
 
 #define JUNCTION_DEVIATION  0.02f // 0.02mm - need to test
 
